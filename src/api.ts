72507dda8a01a4aac82d96e40e7df94963c416c0
Karla210103 <karla.ramirez.gomezz@gmail.com>
2026-07-23 21:41:22 -0600

    backfixed

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const api = axios.create({
   // Forzamos el localhost en el puerto 4000 directamente:
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://back-todo-ten.vercel.app/',
 });
 
 export function setAuth(token: string | null) {
